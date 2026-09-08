3ff63e5e71abd20c915f55e90dcf1918ac892d10
yoyo899843 <yoyo899843@gmail.com>
2026-09-08 13:48:43 +0800

    decrease storage usage

--- MyFirstSecurity - Misc/get-the-root/build/share/run.sh
+++ MyFirstSecurity - Misc/get-the-root/build/share/run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-#exec 2>/dev/null
+exec 2>/dev/null
 cd /home/ctf
-PYTHONWARNINGS="ignore:not adding directory '' to sys.path" timeout 10 ./server.sage
+timeout 10 ./server.py
--- MyFirstSecurity - Misc/get-the-root/build/share/server.py
+++ MyFirstSecurity - Misc/get-the-root/build/share/server.py
@@ -1,6 +1,22 @@
-#!/usr/bin/env sage
+#!/usr/bin/env python3
 import sys
 import random
+
+
+def polymul(a, b):
+    r = [0] * (len(a) + len(b) - 1)
+    for i, x in enumerate(a):
+        for j, y in enumerate(b):
+            r[i + j] += x * y
+    return r
+
+
+def polyeval(coeffs, value):  # coeffs low-degree first
+    acc = 0
+    for c in reversed(coeffs):
+        acc = acc * value + c
+    return acc
+
 
 print("===== Welcome to Get the Root =====")
 print("I need you to solve some polynomial for me, just give me one of the roots of the polynomial")
@@ -11,18 +27,16 @@ print("root : 1 (just one of the roots, and gurantee to be integer)")
 
 sys.stdout.flush()
 
-F.<x> = ZZ[]
-
 for i in range(100):
     print("----- wave : {}/100 -----".format(i + 1))
-    P = 1
+    P = [1]
     for _ in range(random.randint(3, 8)):
-        P *= (x + random.randint(-100, 100))
-    print("polynomial : {}".format(' '.join(map(str, P.list())[::-1])))
+        P = polymul(P, [random.randint(-100, 100), 1])
+    print("polynomial : {}".format(' '.join(map(str, P[::-1]))))
     sys.stdout.write("root : ")
     sys.stdout.flush()
-    root = int(raw_input().strip())
-    if P(root) != 0:
+    root = int(input().strip())
+    if polyeval(P, root) != 0:
         print("WRONG...")
         exit()
 
